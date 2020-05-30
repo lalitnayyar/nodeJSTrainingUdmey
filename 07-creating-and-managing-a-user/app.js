31a5a819972956a0d78d50de675b23ec2a12af20
Lalit <lalitnayyar@gmail.com>
2020-05-30 16:16:47 +0400

    adding existing product and to cart
    adding existing product and to cart

--- 07-creating-and-managing-a-user/app.js
+++ 07-creating-and-managing-a-user/app.js
@@ -1,49 +1,49 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const app = express();
+const errorController = require('./controllers/error')
+const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
   User.findByPk(1)
     .then(user => {
-      req.user = user;
-      next();
+      req.user = user
+      next()
     })
-    .catch(err => console.log(err));
-});
+    .catch(err => console.log(err))
+})
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product,{ through: CartItem});
-Product.belongsToMany(Cart,{ through: CartItem});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem})
+Product.belongsToMany(Cart, { through: CartItem})
 
 sequelize
-   .sync({ force: true })
-  //.sync({logging: console.log})
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
@@ -56,7 +56,13 @@ sequelize
   })
   .then(user => {
     // console.log(user);
-    app.listen(3003);
+    if (!user) {
+    return user.createCart();
+    }
+    return user;
+  })
+  .then(cart => {
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
