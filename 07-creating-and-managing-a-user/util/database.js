f664eefdd6519615cce06a4c05965f93b0ffb863
Lalit <lalitnayyar@gmail.com>
2020-05-30 12:21:51 +0400

    one to many and many to many relationship cart
    one to many and many to many relationship cart

--- 07-creating-and-managing-a-user/util/database.js
+++ 07-creating-and-managing-a-user/util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize').Sequelize;
 const sequelize = new Sequelize('node-complete', 'root', 'Sharda@23', {
   dialect: 'mysql',
   host: 'localhost'
+  //logging: console.log
 });
 
 module.exports = sequelize;
