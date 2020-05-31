90c589c3635a149efd7e9e8a6c78fc91e6a87338
Lalit <lalitnayyar@gmail.com>
2020-05-31 16:41:12 +0400

    updated with mongo db cloud connection
    updated with mongo db cloud connection

--- 07-creating-and-managing-a-user/util/database.js
+++ 07-creating-and-managing-a-user/util/database.js
@@ -1,9 +1,21 @@
-const Sequelize = require('sequelize').Sequelize;
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'Sharda@23', {
-  dialect: 'mysql',
-  host: 'localhost'
-  //logging: console.log
-});
+const mongoConnect = (callback)=>
+{
+  MongoClient.connect(
+    "mongodb+srv://lalitnayyar:Sharda@23@cluster0-ow0zp.mongodb.net/test?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected !!!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
+  
+}
+module.exports= mongoConnect;
 
-module.exports = sequelize;
+
