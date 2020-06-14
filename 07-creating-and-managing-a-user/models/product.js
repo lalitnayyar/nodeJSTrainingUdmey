5ed9a4772049f532ce88e884a07327ca38f1182c
Lalit <lalitnayyar@gmail.com>
2020-06-14 17:38:01 +0400

    update product data in controler
    insertone and updateone with two parameters

--- 07-creating-and-managing-a-user/models/product.js
+++ 07-creating-and-managing-a-user/models/product.js
@@ -2,19 +2,30 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
+    let dbOp;
+    if (this._id){
+      //upadate this product
+      dbOp = db
+      .collection('products')
+      .updateOne({_id: new mongodb.ObjectID(this._id)},{$set: this});
+    } else {
+      dbOp = db
       .collection('products')
       .insertOne(this)
-      .then(result => {
+    }
+
+    return dbOp
+    .then(result => {
         console.log(result);
       })
       .catch(err => {
