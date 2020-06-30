ae8c3d29bba215642bae2ef7b8f2a49b5dc1ee4a
Lalit <lalitnayyar@gmail.com>
2020-06-30 19:05:49 +0400

    update cart
    update cart

--- 07-creating-and-managing-a-user/models/user.js
+++ 07-creating-and-managing-a-user/models/user.js
@@ -15,33 +15,33 @@ class User {
     return db.collection("users").insertOne(this);
   }
 
+  // 
+  
   addToCart(product) {
     const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString();
     });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-    let newQuanity = 1;
-    const updatedCartItems = [...this.cart.item];
     if (cartProductIndex >= 0) {
-      newQuanity = this.cart.items[cartProductIndex].quantity + 1;
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
       updatedCartItems[cartProductIndex].quantity = newQuantity;
     } else {
       updatedCartItems.push({
         productId: new ObjectId(product._id),
-        quantity: newQuanity,
+        quantity: newQuantity
       });
     }
-
-    //product.quantity = 1;
-    const updateCart = {
-      item: updatedCartItems
+    const updatedCart = {
+      items: updatedCartItems
     };
     const db = getDb();
     return db
-      .collection("users")
+      .collection('users')
       .updateOne(
         { _id: new ObjectId(this._id) },
-        { $set: { cart: updateCart } }
+        { $set: { cart: updatedCart } }
       );
   }
   static findById(userId) {
