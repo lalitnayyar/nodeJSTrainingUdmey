4f74675246d7702b848f7ef88cf4c58af0b0b7a5
Lalit <lalitnayyar@gmail.com>
2020-07-05 17:50:45 +0400

    mongoose connection add product
    mongoose connection add product

--- 07-creating-and-managing-a-user/app.js
+++ 07-creating-and-managing-a-user/app.js
@@ -2,9 +2,10 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error')
-const mongoConnect = require('./util/database').mongoConnect;
+//const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
  
@@ -19,21 +20,29 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req, res, next) => {
-  User.findById('5efb3fd9b12628325418c211')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart,user._id);
-      next()
-    })
-    .catch(err => console.log(err))
-})
+// app.use((req, res, next) => {
+//   User.findById('5efb3fd9b12628325418c211')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart,user._id);
+//       next()
+//     })
+//     .catch(err => console.log(err))
+// })
 
- app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+  app.use('/admin', adminRoutes)
+ app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://lalitnayyar:3WHw4TgC@cluster0.1hh10.mongodb.net/shop?retryWrites=true&w=majority'
+    //3 WALMART HULU walmart 4 TOKYO golf COFFEE 
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
