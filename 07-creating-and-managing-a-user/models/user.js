1a0a8226e17fb71311446468ff4d3e5d811deb20
Lalit <lalitnayyar@gmail.com>
2020-07-15 17:47:30 +0400

    224+225 cleaning the cart after storing an order
    224+225 cleaning the cart after storing an order

--- 07-creating-and-managing-a-user/models/user.js
+++ 07-creating-and-managing-a-user/models/user.js
@@ -54,6 +54,10 @@ this.cart.items = updatedCartItems;
 return this.save();
 }
 
+userSchema.methods.clearCart = function(){
+  this.cart = {items: []};
+  return this.save();
+}
 module.exports = mongoose.model('User',userSchema);
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
